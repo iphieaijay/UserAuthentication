7cbc27d152fb581450c6d0760d4b25d718d20e14
ijeoma-tech <ijeoma@storesmart.africa>
2024-12-24 09:59:27 +0100

    Added DbContext

---
+++ UserAuthentication/Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UserAuthentication.Domain.Entities;
+
+namespace UserAuthentication.Infrastructure.Context
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        :IdentityDbContext<ApplicationUser>(options)
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
