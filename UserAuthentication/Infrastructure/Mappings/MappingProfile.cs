804d72b71dfa8c63985264c0ccaa5572cac82124
ijeoma-tech <ijeoma@storesmart.africa>
2024-12-24 09:55:23 +0100

    Added Mappings for Automapper

---
+++ UserAuthentication/Infrastructure/Mappings/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using UserAuthentication.Domain.Contracts;
+using UserAuthentication.Domain.Entities;
+
+namespace UserAuthentication.Infrastructure.Mappings
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ApplicationUser, UserResponse>();
+            CreateMap<ApplicationUser,CurrentUserResponse>();
+            CreateMap<UserRegisterRequest, ApplicationUser>();
+        }
+    }
+}
